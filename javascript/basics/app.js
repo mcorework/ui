44bafe4eafe32afb25116c6b20f500480412e435
mcorework <mcorework@gmail.com>
2021-01-29 20:31:58 -0500

    Added lexical scoping

--- javascript/basics/app.js
+++ javascript/basics/app.js
@@ -1,7 +1,29 @@
-function rant(message){
-    for(i=0; i<2; i++){
-        console.log(message.toUpperCase());
-    }
-}
+//"use strict";
+console.log('Connected to js.');
 
-rant('I have Beets 123');
+// Notes
+// Declare a variable
+
+/***************  1.Global vs Local variables (Lexical scoping) **************
+ * (a) 'use strict' in the beginning to enforce variable declaration. It it is not
+ *      given, the variables which are not declared are global variables.
+ * (b) let vs var, var is function scope and let is block scoped.
+ * 
+ */
+
+var num =8;
+
+(function (){
+    var i=6;
+    console.log(i);
+}());
+console.log('Printing the global variable: '+i);
+
+(function abc(){
+    var k = 1;
+    if(true){
+        var k =3;
+        console.log(k);
+    }
+    console.log(k);
+}());
