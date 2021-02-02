a83d8c7a56a0ea45789d208d78c05a0f97405d12
Manas Samantray <samantraym@gao.gov>
2021-02-02 14:53:17 -0500

    Added Array.from

--- javascript/basics/app.js
+++ javascript/basics/app.js
@@ -102,6 +102,7 @@ if (x) {
 }
 
 /***************  6. Array.from Method in JS ***************/
+/***************  7. Array and Object Performance ***************/
 //To check an inbuilt function we have to use dir.
 //Array has a prototype method called from. (Array.from)
 console.dir(Array);
@@ -117,4 +118,42 @@ const fromScratch3 = new Array(5).fill(0).map((v, i) => i);
 console.log(fromScratch3);
 
 const Ary = [1, 1, 2, 2, 3, 4];
-console.log(Array.from(new Set(Ary)));
+console.log(Array.from(new Set(Ary))); //Set holds all unique values.
+console.log([...new Set(Ary)]);
+
+const func1 = function() {
+  console.log(Array.from(arguments).join("-"));
+};
+func1("m", "a", "n", "a", "s");
+
+// 1. Negative Indexes
+// 2.How they are different from objects
+// 3.How they impact the performances
+console.log("------");
+const ary1 = [1, 2, 3]; //These elements have indexes
+ary1[-1] = 0; //This has key value pair like object
+ary1["What is your name"] = "Manas";
+for (let key in ary1) {
+  //Arrays are behaving like objects (key and values) Arrays are associative arrays.
+  console.log(`${key}:${ary1[key]}`); //Template String
+}
+const ary2 = [1, 2, 3];
+const obj1 = {};
+obj[0] = 1;
+obj[2] = 2;
+obj[3] = 3;
+
+//Array vs Object performance comparision
+console.time("arr");
+const a = [];
+for (let i = 0; i < 100000; i++) {
+  a[i] = i;
+}
+console.timeEnd("arr");
+
+console.time("obj");
+const o = {};
+for (let i = 0; i < 100000; i++) {
+  o[i] = i;
+}
+console.timeEnd("obj");
